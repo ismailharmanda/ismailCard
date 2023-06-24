1f8829a77413bc93394b9b4bc77c704c5a940a96
İsmail Harmanda <iharmanda@hotmail.com>
2023-06-24 15:29:11 +0300

    creat a Z stack, add background color via Color(), ignore safe are via edgesIngoreSafeAre

--- IsmailCard/ContentView.swift
+++ IsmailCard/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color(.systemGreen).edgesIgnoringSafeArea(.all)
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, world!")
+            }
+            .padding()
         }
-        .padding()
+        
     }
 }
 
