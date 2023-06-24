95fe9485c864cbc5d2c975b4b88653e468242dbe
İsmail Harmanda <iharmanda@hotmail.com>
2023-06-24 15:28:21 +0300

    initial start

---
+++ IsmailCard/IsmailCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  IsmailCardApp.swift
+//  IsmailCard
+//
+//  Created by ismail harmanda on 24.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct IsmailCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
