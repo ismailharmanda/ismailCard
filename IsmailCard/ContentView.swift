aff416583b8afaf126b315c34bc42a6aed547e8d
İsmail Harmanda <iharmanda@hotmail.com>
2023-06-24 17:17:50 +0300

    create phone number container

--- IsmailCard/ContentView.swift
+++ IsmailCard/ContentView.swift
@@ -7,21 +7,42 @@
 
 import SwiftUI
 
+let ismailGreen: Color = Color(red: 0.09, green: 0.63, blue: 0.52, opacity: 1)
+
 struct ContentView: View {
     var body: some View {
-        ZStack{
-            Color(red: 0.09, green: 0.63, blue: 0.52, opacity: 1)
+      
+        ZStack {
+        
+            ismailGreen
                 .edgesIgnoringSafeArea(.all)
             VStack {
                 VStack {
                     Image("ismail")
-                        .resizable().aspectRatio(contentMode: .fit).frame(width: 200).clipShape(Circle()).overlay(Circle().stroke(Color.white,lineWidth: 5))
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 200)
+                        .clipShape(Circle())
+                        .overlay(Circle().stroke(Color.white,lineWidth: 5))
                     Text("ismail harmanda")
-                        .font(Font.custom("Pacifico-Regular", size: 40)).bold().foregroundColor(.white)
+                        .font(Font.custom("Pacifico-Regular", size: 40))
+                        .bold()
+                        .foregroundColor(.white)
                     Text("Mobile Developer")
                         .foregroundColor(.white)
                         .font(.system(size: 25))
                     Divider()
+                    RoundedRectangle(cornerRadius: 25)
+                        .padding(.horizontal)
+                        .frame(height:50)
+                        .foregroundColor(.white)
+                        .overlay(HStack {
+                            Image(systemName: "phone.fill")
+                                .foregroundColor(ismailGreen)
+                            Text("+90 532 524 30 11")
+                                .foregroundColor(.black)
+                                .fontWeight(.bold)
+                        },alignment: .center)
                     
                 }
             }
